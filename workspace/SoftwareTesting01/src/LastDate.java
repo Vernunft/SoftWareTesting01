190ef54e354ecb15232d62c375d1e18f9ba9c0fe
Vernunft <libingbing.15@qq.com>
2016-04-05 21:55:31 +0800

    第七周修改的代码

--- workspace/SoftwareTesting01/src/LastDate.java
+++ workspace/SoftwareTesting01/src/LastDate.java
@@ -1,3 +1,5 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Scanner;
 
@@ -10,68 +12,70 @@ public class LastDate {
 		while (year != -1) {
 			System.out.println("请输入年 月  日");
 			year = in.nextInt();
+			if (year == -1)
+			{
+				break;
+			}
 			month = in.nextInt();
 			day = in.nextInt();
-			System.out.println(lastDay(year, month, day));
+			try {
+				System.out.println(lastDay(year, month, day));
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 
 	}
 
-	public static String lastDay(int year, int month, int day) {
-		String lastDay = "";
-		int flag = 0;
-		if (!(year >= 1912 && year <= 2050)) {
-			flag = 1;
-			lastDay += "年份超出范围";
-		} else if (!(month >= 1 && month <= 12)) {
-			flag = 1;
-			lastDay += "月份超出范围";
-		} else if (!(day >= 1 && day <= 31)) {
-			flag = 1;
-			lastDay += "日期超出范围";
+	public static String lastDay(int year, int month, int day) throws ParseException {
+		if (year < 1912 || year > 2050) {
+			return "年份超出范围";
 		}
-		if (flag == 0) {
-			if (month == 2 && day >= 28) {
-				flag = 3;
-				if (((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) && day > 29) {
-					lastDay += year + "年2月只有到29号";
-				} else {
-					lastDay += year + "年2月只有到28号";
+
+		if (month < 1 || month > 12) {
+			return "月份超出范围";
+		}
+
+		if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+			if (month == 2) {
+				if (month < 1 || month > 29) {
+					return "日期超出范围";
+				}
+			} else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10
+					|| month == 12) {
+				if (day < 1 || day > 31) {
+					return "日期超出范围";
+				}
+			} else if (month == 4 || month == 6 || month == 9 || month == 11) {
+				if (day < 1 || day > 30) {
+					return "日期超出范围";
 				}
 			}
-			if (month == 4 || month == 6 || month == 9 || month == 11) {
-				flag = 3;
-				if (day == 31) {
-					lastDay += year + "年" + month + "月只有到30号";
+
+		} else {
+			if (month == 2) {
+				if (day < 1 || day > 28) {
+					return "日期超出范围";
+				}
+			} else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10
+					|| month == 12) {
+				if (day < 1 || day > 31) {
+					return "日期超出范围";
+				}
+			} else if (month == 4 || month == 6 || month == 9 || month == 11) {
+				if (day < 1 || day > 30) {
+					return "日期超出范围";
 				}
 			}
+
 		}
-		if (flag != 3 && flag != 1) {
-			if (day != 1) {
-				day -= 1;
-			} else {
-				if (month == 3) {
-					month -= 1;
-					if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
-						day = 29;
-					} else {
-						day = 28;
-					}
-				} else if (month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
-					month -= 1;
-					day = 30;
-				} else if (month == 1) {
-					year -= 1;
-					month = 12;
-					day = 31;
-				} else {
-					month -= 1;
-					day = 31;
-				}
-			}
-			lastDay += year + "年" + month + "月" + day + "日";
-		}
-		return lastDay;
+		SimpleDateFormat sf = new SimpleDateFormat("yyyy年MM月dd日");
+		Calendar c = Calendar.getInstance();
+		c.setTime(sf.parse(year + "年" + month + "月" + day + "日"));
+		c.add(Calendar.DATE, -1);
+
+		return sf.format(c.getTime());
 	}
 
 }
